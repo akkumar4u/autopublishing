a0ad114bba8e08d569ca65d74f8b90eb9b878dd8
akkumar4u <akkumar@carscommerce.inc>
2026-08-07 18:51:37 +0530

    updated url server log to this https://autopublishing.up.railway.app/
    http://127.0.0.1:8787 replaced with this https://autopublishing.up.railway.app/

--- server.js
+++ server.js
@@ -318,6 +318,6 @@ app.post('/api/wordpress/draft', async (req, res) => {
 app.listen(
   8787,
   () => console.log(
-    'Publishing bridge running at http://127.0.0.1:8787'
+    'Publishing bridge running at https://autopublishing.up.railway.app/'
   )
 );
